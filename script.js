4ac236b8008d2cada70ea08a3ece922104030fb1
Whitessx <89744868+Whitessx@users.noreply.github.com>
2022-05-08 21:59:26 +0200

    Added map functionality to restaurants page

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ HIDE.addEventListener("click", function() {
     MENU.style.display = "none";
     HIDE.style.display = "none";
 });
+
+const BOTON_MAPA = document.querySelector("#map_btn");
+const MAPA = document.querySelector(".iframe_gamarra");
+const GAMARRA_GRID = document.querySelector(".restaurants__gamarra");
+const HIDE_RESTAURANT = document.querySelector(".transparent_restaurant");
+
+MAPA.style.display = "none";
+
+BOTON_MAPA.addEventListener("click", function() {
+  if (MAPA.style.display == "none") {
+    MAPA.style.display = "block";
+    GAMARRA_GRID.style.display = "none";
+    HIDE_RESTAURANT.style.display = "block";
+  } else if (MAPA.style.display == "block") {
+    MAPA.style.display = "none";
+    GAMARRA_GRID.style.display = "grid";
+    HIDE_RESTAURANT.style.display = "none";
+  }
+});
+
+HIDE_RESTAURANT.addEventListener("click", function() {
+  MAPA.style.display = "none";
+  HIDE_RESTAURANT.style.display = "none";
+  GAMARRA_GRID.style.display = "grid";
+});
